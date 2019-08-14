6c9ad59f44fbdbdecf465063725fc2c47b32e013
Corbin <tegnerc@oregonstate.edu>
2019-08-14 14:43:02 -0700

    Create login.php

---
+++ login.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+
+<?php
+	$currentpage = "Login Page";
+
+?>
+<html>
+	<head>
+		<title>Login</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	
+	<header>
+		<h1>Login</h1>
+	</header>
+	
+	<body>
+		<ul>
+			<li><a href="index.html">Home</a></li>
+			<li><a href="shoes.php">Shoes</a></li>
+			<li><a href="suppliers.php">Suppliers</a></li>
+			<li><a href="addShoes.php">Add a Shoe</a></li>
+			<li><a href="addSuppliers.php">Add by Supplier</a></li>
+		</ul>
+
+		<br>
+
+
+		<div class = "container">
+			<form method = "post" action = "">
+				<div>
+					<h4>Enter username and password<h4>
+					<div>
+						<input type = "text" class = "textbox" name = "txt_uname" placeholder = "Username" />
+					</div>
+					<div>
+						<input type = "password" class = "textbox" name = "txt_pwd" placeholder = "Password" />
+					</div>
+					<div>
+						<input type = "submit" value = "Log in" name = "sub_btn" id = "sub_btn" />
+					</div>
+				</div>
+			</form>
+		</div>
+		<?php
+
+			include 'connectvars.php';
+			$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+			if(!$conn)
+			{
+				die('Could not connect: '.mysql_error());
+			}
+	
+			if(isset($_POST['sub_btn']))
+			{
+				$uname = mysqli_real_escape_string($conn, $_POST['txt_uname']);
+				$password = mysqli_real_escape_string($conn, $_POST['txt_pwd']);
+		
+				if($uname != "" && $password != "")
+				{
+					$sql_query = "SELECT COUNT(*) AS cntUser FROM User WHERE username = '".$uname."' AND password = '".$password."'";
+					$result = mysqli_query($conn, $sql_query);
+					$row = mysqli_fetch_array($result);
+				
+					$count = $row['cntUser'];
+				
+					if($count > 0)
+					{
+						$_SESSION['uname'] = $uname;
+						echo "<script type = 'text/javascript'> document.location = 'index.html'; </script>"; //javascript inside php because the header redirect isn't working
+					}
+					else
+					{
+						echo "Invalid username and/or password";
+					}
+				}
+			}
+		?>
+	</body>
+</html>
